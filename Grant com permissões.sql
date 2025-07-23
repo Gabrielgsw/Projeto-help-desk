fb1d159999faff0a8a32337640bf7dcb827791f9
aeciojose <aeciojunior45@gmail.com>
2025-07-23 18:27:00 -0300

    grants com permissoes

--- Grant com permissões.sql
+++ Grant com permissões.sql
@@ -1,29 +1,76 @@
 USE help_desk;
 
--- Um cliente pode registrar chamados, enviar comentários, dar feedback e consultar seus dados
-GRANT SELECT ON Chamado TO CLIENTE;
-GRANT INSERT ON Chamado TO CLIENTE;
-GRANT INSERT, SELECT ON Comentario TO CLIENTE;
-GRANT INSERT, SELECT ON Feedback TO CLIENTE;
+-- Criação das roles
+CREATE ROLE 'cliente_pj';
+CREATE ROLE 'supervisor';
+CREATE ROLE 'tecnico';
 
--- Um técnico pode gerenciar chamados e logs
-GRANT SELECT, UPDATE ON Chamado TO TECNICO;
-GRANT INSERT, SELECT ON Comentario TO TECNICO;
-GRANT INSERT, SELECT ON Historico TO TECNICO;
-GRANT INSERT, SELECT ON Auditoria TO TECNICO;
-GRANT INSERT, SELECT ON LogAtividades TO TECNICO;
-GRANT INSERT, SELECT ON TempoAtividade TO TECNICO;
+-- Criação de usuários fictícios
+CREATE USER 'user_cliente'@'%' IDENTIFIED BY 'senha123';
+CREATE USER 'user_supervisor'@'%' IDENTIFIED BY 'senha123';
+CREATE USER 'user_tecnico'@'%' IDENTIFIED BY 'senha123';
 
--- Um supervisor pode ver tudo e atualizar base de conhecimento
-GRANT SELECT ON * TO SUPERVISOR;
-GRANT INSERT, UPDATE ON BaseConhecimento TO SUPERVISOR;
-GRANT INSERT, UPDATE ON PalavraChave TO SUPERVISOR;
-GRANT ALL PRIVILEGES ON help_desk.* TO SUPERVISOR;
+-- Atribuindo roles
+GRANT 'cliente_pj' TO 'user_cliente'@'%';
+GRANT 'supervisor' TO 'user_supervisor'@'%';
+GRANT 'tecnico' TO 'user_tecnico'@'%';
+
+-- Definindo como padrão
+SET DEFAULT ROLE 'cliente_pj' TO 'user_cliente'@'%';
+SET DEFAULT ROLE 'supervisor' TO 'user_supervisor'@'%';
+SET DEFAULT ROLE 'tecnico' TO 'user_tecnico'@'%';
 
 
-GRANT USAGE ON help_desk.* TO CLIENTE;
-GRANT USAGE ON help_desk.* TO TECNICO;
-GRANT USAGE ON help_desk.* TO SUPERVISOR;
+-- Cliente_pj
+GRANT SELECT, INSERT, UPDATE ON HelpDesk.CLIENTE_PJ TO 'cliente_pj';
+GRANT SELECT ON HelpDesk.KPI TO 'cliente_pj';
+GRANT SELECT, INSERT ON HelpDesk.CHAMADO TO 'cliente_pj';
+GRANT SELECT ON HelpDesk.PARCELA_FATURA TO 'cliente_pj';
+GRANT SELECT ON HelpDesk.FATURA TO 'cliente_pj';
+GRANT SELECT ON HelpDesk.SERVICO TO 'cliente_pj';
+GRANT SELECT ON HelpDesk.ORDEM_SERVICO TO 'cliente_pj';
+GRANT SELECT ON HelpDesk.COMPUTADOR TO 'cliente_pj';
+GRANT SELECT ON HelpDesk.IMPRESSORA TO 'cliente_pj';
+GRANT SELECT ON HelpDesk.CONTRATO TO 'cliente_pj';
+GRANT SELECT ON HelpDesk.Envolveu_Ordem_Servico_Computador TO 'cliente_pj';
+GRANT SELECT ON HelpDesk.Envolveu_Ordem_Servico_Impressora TO 'cliente_pj';
 
--- Para que as alterações de privilégio entrem em vigor imediatamente
-FLUSH PRIVILEGES;
+
+
+-- Supervisor
+GRANT SELECT, UPDATE ON HelpDesk.UNIDADE_SUPORTE TO 'supervisor';
+GRANT SELECT ON HelpDesk.TECNICO TO 'supervisor';
+GRANT SELECT ON HelpDesk.CLIENTE_PJ TO 'supervisor';
+GRANT SELECT ON HelpDesk.KPI TO 'supervisor';
+GRANT SELECT, UPDATE ON HelpDesk.CHAMADO TO 'supervisor';
+GRANT SELECT ON HelpDesk.PARCELA_FATURA TO 'supervisor';
+GRANT SELECT ON HelpDesk.FATURA TO 'supervisor';
+GRANT SELECT ON HelpDesk.ORCAMENTO TO 'supervisor';
+GRANT SELECT ON HelpDesk.ORDEM_SERVICO TO 'supervisor';
+GRANT SELECT ON HelpDesk.SERVICO TO 'supervisor';
+GRANT SELECT ON HelpDesk.TIPO_SERVICO TO 'supervisor';
+GRANT SELECT ON HelpDesk.CONTRATO TO 'supervisor';
+GRANT SELECT ON HelpDesk.COMPUTADOR TO 'supervisor';
+GRANT SELECT ON HelpDesk.IMPRESSORA TO 'supervisor';
+GRANT SELECT ON HelpDesk.COMPONENTE TO 'supervisor';
+GRANT SELECT ON HelpDesk.Possui_computador_componente TO 'supervisor';
+GRANT SELECT ON HelpDesk.Envolveu_Ordem_Servico_Computador TO 'supervisor';
+GRANT SELECT ON HelpDesk.Envolveu_Ordem_Servico_Impressora TO 'supervisor';
+
+
+-- Técnico
+GRANT SELECT, UPDATE ON HelpDesk.CHAMADO TO 'tecnico';
+GRANT SELECT ON HelpDesk.KPI TO 'tecnico';
+GRANT SELECT ON HelpDesk.SERVICO TO 'tecnico';
+GRANT SELECT ON HelpDesk.CONTRATO TO 'tecnico';
+GRANT SELECT ON HelpDesk.FATURA TO 'tecnico';
+GRANT SELECT ON HelpDesk.PARCELA_FATURA TO 'tecnico';
+GRANT SELECT ON HelpDesk.COMPUTADOR TO 'tecnico';
+GRANT SELECT ON HelpDesk.IMPRESSORA TO 'tecnico';
+GRANT SELECT ON HelpDesk.DRIVER_IMPRESSORA TO 'tecnico';
+GRANT SELECT ON HelpDesk.COMPONENTE TO 'tecnico';
+GRANT SELECT ON HelpDesk.Possui_computador_componente TO 'tecnico';
+GRANT SELECT ON HelpDesk.Envolveu_Ordem_Servico_Computador TO 'tecnico';
+GRANT SELECT ON HelpDesk.Envolveu_Ordem_Servico_Impressora TO 'tecnico';
+
+-- 
