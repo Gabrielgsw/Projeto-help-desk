6c1bacdd59bfbd1868d6225a96fa4f257aa5ae2d
Gabriel Wanderley <113439146+Gabrielgsw@users.noreply.github.com>
2025-07-26 12:43:05 -0300

    inserindo trigger kpi

--- triggers.sql
+++ triggers.sql
@@ -45,6 +45,63 @@ END //
 DELIMITER ;
 
 
+ -- DROP TRIGGER trigger_kpi_chamados
+DELIMITER //
+
+CREATE TRIGGER trigger_kpi_chamados
+AFTER UPDATE ON SERVICO
+FOR EACH ROW
+BEGIN
+    DECLARE v_matric_tec INT;
+    DECLARE v_kpi_sequencial INT; -- Sequencial para "Chamados Fechados"
+
+    -- Verifica se o status do serviço mudou para 'CONCLUIDO'
+    IF OLD.status != 'Concluído' AND NEW.status = 'Concluído' THEN
+        -- Obtém a matrícula do técnico responsável por este serviço
+        SELECT T.Matricula
+        INTO v_matric_tec
+        FROM TECNICO AS T
+        JOIN CHAMADO AS C ON T.Matricula = C.mat_tec
+        JOIN ORDEM_SERVICO AS OS ON C.Seq = OS.cod_chamado
+        WHERE OS.numero = NEW.num_serv LIMIT 1;
+        
+        SELECT Sequencial INTO v_kpi_sequencial
+        FROM KPI
+        WHERE matric_tec = v_matric_tec AND KPI_1 = 'Chamados Fechados' LIMIT 1;
+
+        -- Se o KPI foi encontrado e o tecnico existe
+        IF v_matric_tec IS NOT NULL AND v_kpi_sequencial IS NOT NULL THEN
+            -- atualiza valor correspondente 
+            UPDATE VALOR_KPI
+            SET valor_kpi_numerico = valor_kpi_numerico + 1
+            WHERE matric_tec = v_matric_tec
+              AND kpi_sequencial = v_kpi_sequencial
+              AND data_registro = CURDATE(); -- Se o KPI for por dia, por exemplo
+
+            -- Se não atualizou nenhuma linha (ou seja, não existe registro para hoje), insere
+            IF ROW_COUNT() = 0 THEN
+                INSERT INTO VALOR_KPI (matric_tec, kpi_sequencial, data_registro, valor_kpi_numerico)
+                VALUES (v_matric_tec, v_kpi_sequencial, CURDATE(), 1);
+            END IF;
+        END IF;
+    END IF;
+END //
+
+DELIMITER ;
+
+-- início teste trigger kpi
+
+SELECT * FROM VALOR_KPI WHERE matric_tec = 101 AND kpi_sequencial = 2;
+
+UPDATE SERVICO
+SET status = 'Concluído'
+WHERE num_serv = 1;
+
+SELECT * FROM SERVICO WHERE num_serv = 1
+
+-- fim do teste trigger kpi
+
+
 -- Trigger para atualizar dt_Devida
 
 DELIMITER ;
